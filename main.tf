446f8a1853afbee7fb06963e4b5f20467204613b
forsmans <magnus.forsman@xenit.se>
2025-01-31 13:39:39 +0100

    terraform version adjustment

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 1.8"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.107"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
   }
